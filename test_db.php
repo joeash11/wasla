7d2b84d40f4429059601fea77cea65c2c0d96455
joeash11 <josephashraf2004@gmail.com>
2026-05-02 00:57:47 +0300

    clean database without redundant data

--- test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/db/connection.php';
-$result = $conn->query("SELECT * FROM smtp_settings ORDER BY id ASC LIMIT 1");
-$row = $result->fetch_assoc();
-var_dump($row);
-var_dump(empty($row['smtp_username']));
-var_dump(empty($row['smtp_password']));
-?>
--- test_email.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/db/connection.php';
-require_once __DIR__ . '/includes/mailer.php';
-$res = sendVerificationEmail('josephashraf2004@gmail.com', 'Joseph', '123456');
-echo $res ? 'Success' : 'Failed';
-?>
